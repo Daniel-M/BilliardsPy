0db144d5e981e6a11038b084f174a1455b1fbebf
Daniel-M <danielmejia55@gmail.com>
2016-03-07 01:36:25 -0500

    Adding bones for Frontier class for boundary

---
+++ Frontier.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+import random
+
+class Frontier:
+    'Main class to define billiard balls objects'
+
+    def __init__(self,mass,radious,position,velocity):
+        self.Mass=mass
+        self.Radious=radious
+        self.Position=position
+        self.Velocity=velocity
+
+    def __str__(self):
+        return "Ball mass: %f" % (self.Mass)
+
+    def getMass(self):
+        return self.Mass
+
+    def getPosition(self):
+        return self.Position
+    
+    def getVelocity(self):
+        return self.Velocity
+
+    def move(self,delta):
+        random.seed()
+        variation =  random.uniform(-0.1*delta,0.1*delta)
+        self.Position = self.Position + delta*self.Velocity + variation
+
+    def reverseVelocity(self,component):
+        random.seed()
+        variation = random.uniform(-0.01*self.Velocity[component],0.01*self.Velocity[component])
+        self.Velocity[component] = -1.0*self.Velocity[component] + variation
+
+
+    def collide(self,limits):
+        if( 0 >= self.Position[0]+self.Radious):
+            self.reverseVelocity(0)
+        elif(self.Position[0]+self.Radious >= limits[0]):
+            self.reverseVelocity(0)
+        elif( 0 > self.Position[1]+self.Radious):
+            self.reverseVelocity(1)
+        elif(self.Position[1]+self.Radious >= limits[1]):
+            self.reverseVelocity(1)
+
+
